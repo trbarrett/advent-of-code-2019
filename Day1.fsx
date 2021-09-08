11f150e6f9a8098f79469e7ba85fa1fe2fa6ecb9
Tristian Barrett <trbarrett@gmail.com>
2021-09-08 17:29:25 +0100

    Complete day 1

---
+++ Day1.fsx
@@ -0,0 +1,28 @@
+#load "./Helper.fsx"
+
+let floor: float -> float = System.Math.Floor
+
+let numbers = Helper.readInput "day1.txt" |> Seq.map int |> Set
+
+let rec extraFuelCalc fuel acc =
+    if fuel <= 0
+    then acc
+    else
+        let extra = (fuel / 3) - 2
+        let extra = System.Math.Max(extra, 0)
+        extraFuelCalc extra (acc + extra)
+let part1 =
+    numbers
+    |> Seq.map (fun x -> (x / 3) - 2)
+    |> Seq.sum
+
+let part2 =
+    numbers
+    |> Seq.map (fun x -> (x / 3) - 2)
+    |> Seq.map (fun x -> x + extraFuelCalc x 0)
+    |> Seq.sum
+
+
+printfn $"Part 1: {part1}"
+
+printfn $"Part 2: {part2}"
--- Day1Part1.fsx
+++
@@ -1,9 +0,0 @@
-#load "./Helper.fsx"
-open Helper
-let numbers = Helper.readInput "day1.txt" |> Seq.map int |> Set
-let result =
-    numbers
-    |> Seq.map (fun x -> (x / 3) - 2)
-    |> Seq.sum
-
-printfn $"{result}"
