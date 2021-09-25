8fdc297f78f0a13049c2d969072f5d653d712165
Tristian Barrett <trbarrett@gmail.com>
2021-09-25 17:52:08 +0100

    Complete day 7 part 2
    Large design change. We now keep track of program state, and suspend
    after each output. Output, and termination is handled by events, and we
    can resume when we need to.

--- Day7.fsx
+++ Day7.fsx
@@ -1,13 +1,56 @@
 #load "./Helper.fsx"
 
-// Day 5 - For 5 intcode computers hocked up in a row, determine the best
-//         "phase setting" input to get the lastget output
+// Day 7 - For five intcode computers hocked up in a row, determine the best
+//         "phase setting" input to get the largest output.
+//         With Part 2 we hook the computers up so they form a feedback loop
+//
+// Method : Each "intcode program" returns control back to the main program
+//          after an IO event, alowing the main program to feed output from
+//          one program to the next. Each program's state is stored when it
+//          becomes suspended, allowing us to restart it whenever we need.
+//
+// Other solutions:
+//         We could have used F# MailBoxProcessor to acutaly run the processors
+//         in parallel and pass events that way. Similarly we could have used
+//         another actor framework, or multiple threads with some type of
+//         Pub/Sub to achive the same thing. Given we only run one program at a
+//         time that would leave most threads waiting though.
 
 
 open Helper
 open System
+open System.Collections.Generic
 
 module IntCodeComputer =
+    type ProgramMemory = string[]
+    type InstructionPointer = int
+
+    type InputStream = Queue<int>
+
+    type RunningProgram =
+        { Memory : ProgramMemory
+          Pointer : InstructionPointer
+          Input : InputStream }
+
+    let initializeProgram programCode (inputData : int seq) =
+        { Memory = Array.copy programCode
+          Pointer = 0
+          Input = Queue<int>(inputData) }
+
+    let saveRunningProgram mem pointer input =
+        { Memory = mem
+          Pointer = pointer
+          Input = input }
+
+    type Event =
+        | Terminate
+        | SuspendUntilNextInput of RunningProgram
+        | PerformOutput of int * RunningProgram
+
+    type ProgramState =
+        | Running of RunningProgram
+        | SuspendedWaitingInput of RunningProgram
+        | Terminated
 
     type Mode =
         | PositionMode // 0
@@ -61,7 +104,7 @@ module IntCodeComputer =
             | 99 -> Exit
             | x -> Unknown x
 
-    let rec execute (input : int list) (mem : string[]) (pos : int) (output : int list) =
+    let rec execute (input : InputStream) (mem : string[]) (pos : int) =
        let paramValue paramNo paramMode =
            if paramMode = PositionMode
            then int mem.[int mem.[pos + paramNo]]
@@ -76,7 +119,7 @@ module IntCodeComputer =
            let writeLoc = int mem.[pos + 3]
            mem.[writeLoc] <- string (inAVal + inBVal)
            //printfn $"Result: {mem.[writeLoc]} at pos: {writeLoc}"
-           execute input mem (pos + (Opcode.instrMemCount opCode)) output
+           execute input mem (pos + (Opcode.instrMemCount opCode))
 
        | Mul (p1mode, p2mode) ->
            //printfn $"Pos: {pos} - Performing Mul"
@@ -85,20 +128,26 @@ module IntCodeComputer =
            let writeLoc = int mem.[pos + 3]
            mem.[writeLoc] <- string (inAVal * inBVal)
            //printfn $"Result: {mem.[writeLoc]} at pos: {writeLoc}"
-           execute input mem (pos + (Opcode.instrMemCount opCode)) output
+           execute input mem (pos + (Opcode.instrMemCount opCode))
 
        | Input ->
            //printfn $"Pos: {pos} - Performing Input"
-           let writeLoc = int mem.[pos + 1]
-           mem.[writeLoc] <- string (List.head input)
-           //printfn $"Result: {mem.[writeLoc]} at pos: {writeLoc}"
-           execute (List.tail input) mem (pos + (Opcode.instrMemCount opCode)) output
+
+           if input.Count > 0
+           then
+               let writeLoc = int mem.[pos + 1]
+               mem.[writeLoc] <- string (input.Dequeue ())
+               //printfn $"Result: {mem.[writeLoc]} at pos: {writeLoc}"
+               execute input mem (pos + (Opcode.instrMemCount opCode))
+           else
+               SuspendUntilNextInput (saveRunningProgram  mem pos input)
 
        | Output pMode ->
            //printfn $"Pos: {pos} - Performing Output"
            let outVal = paramValue 1 pMode
            //printfn $"Output: {outVal}"
-           execute input mem (pos + (Opcode.instrMemCount opCode)) (outVal :: output)
+           let nextPos = (pos + (Opcode.instrMemCount opCode))
+           PerformOutput (outVal, (saveRunningProgram  mem nextPos input))
 
        | JumpIfTrue (p1mode, p2mode)  ->
            //printfn $"Pos: {pos} - Performing Jump If True"
@@ -107,7 +156,7 @@ module IntCodeComputer =
                if inAVal <> 0
                then paramValue 2 p2mode
                else (pos + (Opcode.instrMemCount opCode))
-           execute input mem nextPos output
+           execute input mem nextPos
 
        | JumpIfFalse (p1mode, p2mode)  ->
            //printfn $"Pos: {pos} - Performing Jump If False"
@@ -116,65 +165,130 @@ module IntCodeComputer =
                if inAVal = 0
                then paramValue 2 p2mode
                else (pos + (Opcode.instrMemCount opCode))
-           execute input mem nextPos output
+           execute input mem nextPos
 
        | LessThan (p1mode, p2mode) ->
            let inAVal = paramValue 1 p1mode
            let inBVal = paramValue 2 p2mode
            let writeLoc = int mem.[pos + 3]
            mem.[writeLoc] <- if inAVal < inBVal then "1" else "0"
-           execute input mem (pos + (Opcode.instrMemCount opCode)) output
+           execute input mem (pos + (Opcode.instrMemCount opCode))
 
        | Equals (p1mode, p2mode) ->
            let inAVal = paramValue 1 p1mode
            let inBVal = paramValue 2 p2mode
            let writeLoc = int mem.[pos + 3]
            mem.[writeLoc] <- if inAVal = inBVal then "1" else "0"
-           execute input mem (pos + (Opcode.instrMemCount opCode)) output
+           execute input mem (pos + (Opcode.instrMemCount opCode))
 
        | Exit ->
            //printfn $"Pos: {pos} - Exit"
-           output |> List.rev
+           Terminate
 
        | Unknown x ->
            failwithf $"Unknown opcode {x} at position {pos}"
 
-    let runProgram programCode input =
-        let programCode = Array.copy programCode
-        execute input programCode 0 []
+    let runProgramFromState prog input =
+       match prog with
+       | Running p ->
+           match input with
+           | Some input -> p.Input.Enqueue input
+           | None -> ()
 
-let rec runSeries program phaseSettings curr =
-    match phaseSettings with
-    | [] -> curr
-    | x::xs ->
-        let nextInput = program [x; curr] |> List.last
-        runSeries program xs nextInput
+           execute p.Input p.Memory p.Pointer
+       | SuspendedWaitingInput p ->
+           match input with
+           | Some input -> p.Input.Enqueue input
+           | None -> failwith "No input for program expecting input"
 
-let testAmpHookup program phaseSettings =
-    runSeries program phaseSettings 0
+           execute p.Input p.Memory p.Pointer
+       | Terminated ->
+           Terminate
+
+
+open IntCodeComputer
+
+let prepareSeries program phaseSettings =
+    phaseSettings
+    |> List.map (fun phaseSetting ->
+        initializeProgram program [phaseSetting]
+        |> SuspendedWaitingInput)
+
+let rec runSeries activePrograms input =
+    match activePrograms with
+    | [] -> input // return the final input
+    | prog::rest ->
+        let event = runProgramFromState prog input
+        match event with
+        | Terminate ->
+            // go to the next program in the series
+            runSeries rest input
+        | SuspendUntilNextInput _ ->
+            runSeries rest None
+        | PerformOutput (output, _) ->
+            // run the next program, using this outcome as the next
+            // programs input since they are hooked up in series
+            runSeries rest (Some output)
+
+// The difference between this and `runSeries` is that after running a program
+// that doesn't terminate, we add it to the back of the list of active programs
+// so it can repeat
+let rec runSeriesWithFeedback activePrograms input =
+    match activePrograms with
+    | [] -> input // return the final input
+    | prog::rest ->
+        let event = runProgramFromState prog input
+        match event with
+        | Terminate ->
+            // go to the next program in the series
+            runSeriesWithFeedback rest input
+        | SuspendUntilNextInput p ->
+            // I don't think this is actually possible with our current problem
+            // set. A program is expected to output something before consuming it's
+            // next input
+            runSeriesWithFeedback (rest@[SuspendedWaitingInput p]) None
+        | PerformOutput (output, p) ->
+            // run the next program, using this outcome as the next
+            // programs input since they are hooked up in series
+            runSeriesWithFeedback (rest@[Running p]) (Some output)
+
+let runBasicAmpHookup program phaseSettings =
+    let preparedPrograms = prepareSeries program phaseSettings
+    runSeries preparedPrograms (Some 0)
+
+let runFeedbackAmpHookup program phaseSettings =
+    let preparedPrograms = prepareSeries program phaseSettings
+    runSeriesWithFeedback preparedPrograms (Some 0)
+
 let programCode =
     Helper.readUncommentedLines "day7.txt"
     |> Seq.item 0
     |> String.split ','
 
-let program = IntCodeComputer.runProgram programCode
-
-let bruteForce program =
-    // try every combination of 0 to 4
-    let possiblePhaseSettings = List.permutations (Set [0;1;2;3;4]) 5
-    possiblePhaseSettings
-    |> List.map (testAmpHookup program)
+let bruteForceAllPhaseSettings program phaseOptions ampHookup =
+    let possiblePhaseSettings = List.permutations (Set phaseOptions) 5
+    possiblePhaseSettings // try every combination of phaseOption
+    |> List.map (ampHookup program)
     |> List.zip possiblePhaseSettings
+    |> List.choose (fun (phaseSetting, output) -> output |> Option.map (fun o -> phaseSetting, o))
     //|> List.map (tee (fun (xs, result) -> printfn "[%s] - %d" (String.Join(';', xs)) result ))
     |> List.maxBy snd
 
-let sw = System.Diagnostics.Stopwatch.StartNew ()
-//let part1Result = testAmpHookup program [1;0;4;3;2]
-let phaseSetting, total = bruteForce program
-let phaseSettingStr = sprintf "[%s]" (String.Join(';', phaseSetting))
-printfn $"Part1 result: {phaseSettingStr} - {total}, took: {sw.ElapsedMilliseconds}ms"
+let part1 () =
+    let sw = System.Diagnostics.Stopwatch.StartNew ()
+    let phaseSetting, total =
+        bruteForceAllPhaseSettings programCode [0;1;2;3;4] runBasicAmpHookup
+    let phaseSettingStr = sprintf "[%s]" (String.Join(';', phaseSetting))
+    printfn $"Part1 result: {phaseSettingStr} - {total}, took: {sw.ElapsedMilliseconds}ms"
+    // Correct Answer: [2;3;1;0;4] - 67023
 
-sw = System.Diagnostics.Stopwatch.StartNew ()
-//printfn $"Part 2: (Diagnostic 5)"
-//runDiagnostic5 startingMem |> ignore
-//printfn $"Part2 complete took: {sw.ElapsedMilliseconds}ms"
+let part2 () =
+    let sw = System.Diagnostics.Stopwatch.StartNew ()
+    let phaseSetting, total =
+        bruteForceAllPhaseSettings programCode [5;6;7;8;9] runFeedbackAmpHookup
+    let phaseSettingStr = sprintf "[%s]" (String.Join(';', phaseSetting))
+    printfn $"Part2 result: {phaseSettingStr} - {total}, took: {sw.ElapsedMilliseconds}ms"
+    // Correct Answer: [5;Day72.fsx8;7;9;6] - 7818398
+
+part1 ()
+part2 ()
